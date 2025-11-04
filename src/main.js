b3a3badd4f228cee12b39e508c69688a72ac4928
Nicholas <nicholasang.sg@gmail.com>
2025-11-04 13:14:48 +0800

    save

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './assets/navbar.css'
 /* Google Maps API */
 (function() {
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}&libraries=places,marker&v=weekly&callback=Function.prototype`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}&libraries=places,marker&v=weekly&callback=Function.prototype&loading=async`;
   script.async = true;
   script.defer = true;
   document.head.appendChild(script);
